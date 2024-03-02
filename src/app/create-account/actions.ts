8af929a4606f99248cd01c15cec6638e303fc9d5
jinwook song <wlsdnr129@naver.com>
2024-03-02 19:58:47 +0900

    feat(validation): zod refinement & custom error message

--- src/app/create-account/actions.ts
+++ src/app/create-account/actions.ts
@@ -2,12 +2,32 @@
 
 import { z } from 'zod';
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirmPassword: z.string().min(10),
-});
+interface CheckConfirmPasswordProps {
+  password: string;
+  confirmPassword: string;
+}
+
+const checkUsername = (username: string) => username.includes('carrot');
+const checkConfirmPassword = ({
+  password,
+  confirmPassword,
+}: CheckConfirmPasswordProps) => password == confirmPassword;
+
+const formSchema = z
+  .object({
+    username: z
+      .string()
+      .min(3, 'Too short!')
+      .max(10, 'Too long!')
+      .refine(checkUsername, 'custom error'),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirmPassword: z.string().min(10),
+  })
+  .refine(checkConfirmPassword, {
+    message: 'Both password should be the same',
+    path: ['confirmPassword'],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -19,6 +39,7 @@ export async function createAccount(prevState: any, formData: FormData) {
 
   const result = formSchema.safeParse(data);
   if (!result.success) {
+    console.log(result.error.flatten());
     return result.error.flatten();
   }
 }
