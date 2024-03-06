9eabc738add4f8a0214270119edd881fc7374a8c
jinwook song <wlsdnr129@naver.com>
2024-03-06 22:49:10 +0900

    feat(authenticate): create & login user

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -5,10 +5,26 @@ import {
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
 } from '@/libs/constants';
+import db from '@/libs/db';
 import { z } from 'zod';
+import bcrypt from 'bcrypt';
+import getSession from '@/libs/session';
+import { redirect } from 'next/navigation';
+
+const checkEmailExists = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: { email },
+  });
+
+  return Boolean(user);
+};
 
 const formSchema = z.object({
-  email: z.string().email().toLowerCase(),
+  email: z
+    .string()
+    .email()
+    .toLowerCase()
+    .refine(checkEmailExists, 'An account with this email does not exist.'),
   password: z
     .string()
     .min(PASSWORD_MIN_LENGTH)
@@ -21,11 +37,30 @@ export async function login(prevState: any, formData: FormData) {
     password: formData.get('password'),
   };
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
 
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    const { email, password } = result.data;
+    const user = (await db.user.findUnique({
+      where: { email },
+      select: { id: true, password: true },
+    }))!;
+
+    const matched = await bcrypt.compare(password, user.password ?? '');
+    if (matched) {
+      const session = await getSession();
+      session.id = user.id;
+      await session.save();
+      redirect('/');
+    }
+
+    return {
+      fieldErrors: {
+        email: [],
+        password: ['Wrong password.'],
+      },
+    };
   }
 }
