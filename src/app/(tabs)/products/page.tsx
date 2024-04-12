fc6ae601bc833a6d21707ad60d6f072b5063a41c
jinwook song <wlsdnr129@naver.com>
2024-04-12 13:26:38 +0900

    feat(product): list component

--- src/app/(tabs)/products/page.tsx
+++ src/app/(tabs)/products/page.tsx
@@ -1,8 +1,27 @@
+import ProductList from '@/components/ProductList';
+import db from '@/libs/db';
+
 async function getProducts() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+      title: true,
+      price: true,
+      createdAt: true,
+      photo: true,
+    },
+  });
+  return products;
 }
 
 export default async function Products() {
   const products = await getProducts();
-  return <div>Products</div>;
+  return (
+    <div className='flex flex-col gap-5 p-5'>
+      {products.map((product) => (
+        <ProductList key={product.id} {...product} />
+      ))}
+    </div>
+  );
 }
---
+++ src/components/ProductList.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface ProductListProps {
+  id: number;
+  title: string;
+  price: number;
+  photo: string;
+  createdAt: Date;
+}
+
+export default function ProductList({
+  id,
+  title,
+  price,
+  photo,
+  createdAt,
+}: ProductListProps) {
+  return (
+    <Link href={`products/${id}}`} className='flex gap-5'>
+      <div className='relative size-28 rounded-md overflow-hidden'>
+        <Image src={photo} alt={title} fill />
+      </div>
+      <div className='flex flex-col gap-1 *:text-white'>
+        <span className='text-lg'>{title}</span>
+        <span className='text-sm text-neutral-500'>
+          {createdAt.toISOString()}
+        </span>
+        <span className='text-lg font-semibold'>${price}</span>
+      </div>
+    </Link>
+  );
+}
