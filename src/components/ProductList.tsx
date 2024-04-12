4db5539620bb0f228e60c4f233c39f335b936fa7
jinwook song <wlsdnr129@naver.com>
2024-04-13 02:31:01 +0900

    feat(products): infinite scroll with IntersectionObserver

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import ProductItem from './ProductItem';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { SimpleProducts, fetchProducts } from '@/app/(tabs)/products/actions';
 
 interface ProductListProps {
@@ -13,6 +13,35 @@ export default function ProductList({ initialProducts }: ProductListProps) {
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(0);
   const [isLastPage, setIsLastPage] = useState(false);
+
+  const trigger = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      async (
+        entries: IntersectionObserverEntry[],
+        observer: IntersectionObserver
+      ) => {
+        const isIntersecting = entries[0].isIntersecting;
+        if (isIntersecting && trigger.current) {
+          observer.unobserve(trigger.current);
+          await handleLoad();
+        }
+      },
+      {
+        threshold: 0.5,
+        rootMargin: '0px 0px -100px 0px',
+      }
+    );
+    if (trigger.current) {
+      observer.observe(trigger.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
 
   const handleLoad = async () => {
     setIsLoading(true);
@@ -26,20 +55,25 @@ export default function ProductList({ initialProducts }: ProductListProps) {
     }
     setIsLoading(false);
   };
+
   return (
     <div className='flex flex-col gap-5 p-5'>
       {products.map((product) => (
         <ProductItem key={product.id} {...product} />
       ))}
       {!isLastPage && (
-        <div className='w-fit mx-auto active:scale-95 transition-transform'>
-          <button
-            onClick={handleLoad}
-            disabled={isLoading}
+        <div
+          style={{
+            marginTop: `${(page + 1) * 300}vh`,
+          }}
+          className='w-fit mb-40 mx-auto active:scale-95 transition-transform'
+        >
+          <span
+            ref={trigger}
             className='primary-btn whitespace-nowrap px-4 py-2'
           >
             {isLoading ? 'Loading...' : 'Load more'}
-          </button>
+          </span>
         </div>
       )}
     </div>
